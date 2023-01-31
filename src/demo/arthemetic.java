854d84d422811595f04df13619f10848bcc17399
Anil19113051 <anilekkarala@gmail.com>
2023-01-31 22:41:22 +0530

    commiting the files

---
+++ src/demo/arthemetic.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class arthemetic {
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main( String [] args) {
+        arthemetic art = new arthemetic();
+        System.out.println("addtion of 5,10 is " + art.add(5, 10));
+    }
+    }
+
---
+++ src/demo/decision.java
@@ -0,0 +1,13 @@
+package com.demo;
+import java.io.*;
+
+public class decision {
+    public static void main(String[] args){
+        int x= 1;
+        int y= 20;
+        if(x>y)
+            System.out.println("false");
+        else
+            System.out.println("its  true");
+    }
+}
