bba5fd72dbffa266ddcffa6ab1320ed04b2dbf72
Anil19113051 <anilekkarala@gmail.com>
2023-02-03 14:51:26 +0530

    commiting the is code

---
+++ data/Decisiondemo.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Decisiondemo {
+    public static void main(String[] args){
+        int x = 10 ; int y = 20;
+        if (x<y)
+            System.out.println("False");
+        else
+            System.out.println("True");
+        System.out.println("outside the if condition");
+    }
+}
+
+
+
